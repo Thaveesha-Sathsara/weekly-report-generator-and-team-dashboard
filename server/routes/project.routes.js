25fdc73fdd90d021dae4a507dddc68ffd380ee18
Thaveesha Vithana <t.s.vithana@gmail.com>
2026-07-09 14:05:17 +0530

    update propject routes

--- server/routes/project.routes.js
+++ server/routes/project.routes.js
@@ -4,12 +4,14 @@ const {
     createProject,
     getProjects,
     updateProject,
-    deleteProject
+    deleteProject,
+    getProjectById
 } = require('../controllers/project.controller');
 const { protect, managerOnly } = require('../middleware/auth.middleware');
 
 router.post('/', protect, managerOnly, createProject);
 router.get('/', protect, getProjects);
+router.get('/:id', protect, getProjectById);
 router.put('/:id', protect, managerOnly, updateProject);
 router.delete('/:id', protect, managerOnly, deleteProject);
 
